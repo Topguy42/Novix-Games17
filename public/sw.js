38040bcd492bb8e998e67831087d57658f9678e3
PeteZah <petezahiscool@gmail.com>
2025-11-03 21:58:53 -0500

    ads

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,10 @@
+self.options = {
+    "domain": "3nbf4.com",
+    "zoneId": 10138156
+}
+self.lary = ""
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+
 if (navigator.userAgent.includes("Firefox")) {
 	Object.defineProperty(globalThis, "crossOriginIsolated", {
 		value: true,
